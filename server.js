d9295a9c96253808a35193707945171997715e2e
Gabriel Jorge <gabrielbielcruz1.007@gmail.com>
2023-01-23 15:42:16 -0300

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res)=>{res.sendFile('index.html', { root: __dirname });});
+app.get('/script.js', (req, res)=>{res.sendFile('script.js', { root: __dirname });});
+app.listen(8080, function()
+{
+	console.log("When Server Starts, Log This");
+});
